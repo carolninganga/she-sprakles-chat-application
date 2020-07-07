d603bca67d8cb861084540d3ca607289d7fef9ca
Caroline Ninganga <carolninganga@gmail.com>
2020-07-07 11:19:09 -0400

    imported files in App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Chat from './Pages/Chat/Chat';
+import Signup from './Pages/Login/Login';
+import firebase from './Services/firebase';
+import { toast, ToastContainer } from 'react-toastify';
 
-function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+class App extends Component {
+
+  showToast = (type, message) => {
+    switch (type) {
+      case 0:
+        toast.warning(message)
+        break;
+        case 1:
+          toast.success(message)
+    }
+  }
 }
 
-export default App;
+
+
