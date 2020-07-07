e5e34b4e63bfea8a3ca90583539d5701d5ba1446
Caroline Ninganga <carolninganga@gmail.com>
2020-07-07 11:28:41 -0400

    added componentDidmout in App file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Redirect
 } from 'react-router-dom';
+import './App.css';
 import Home from './Pages/Home/Home';
 import Chat from './Pages/Chat/Chat';
 import Signup from './Pages/Login/Login';
@@ -20,7 +21,39 @@ class App extends Component {
         break;
         case 1:
           toast.success(message)
+          default:
+            break;
     }
+
+    constructor(){
+      super();
+      this.state = {
+        authenticated: false,
+        loading: true
+      };
+    }
+
+    componentDidMount(){
+      firebase.auth().onAuthStateChanged(user => {
+        if(user){
+          this.setState({
+            authenticated : true,
+            loading: false
+          });
+
+        } else {
+          this.setState({
+            authenticated: false,
+            loading: false
+          });
+        }
+      })
+    }
+      render(){
+        return()
+      }
+    }
+
   }
 }
 
