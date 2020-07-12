a1491642fedc4e4a37cac0aeeb8152a3baec6bf9
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-12 10:52:05 -0400

    added inline styling in login file

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -15,7 +15,56 @@ import Typography from "@material-ui/core/Typography";
 
 
 export default class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            isLoading: true,
+            error: null,
+            email: "",
+            password: "",
+
+        }
+    }
     render() {
+        const paper = {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            paddingLeft: '10px',
+            paddingRight: '10px'
+        }
+        const rightcomponent={
+            boxShadow: '0 80px #8088888',
+            backgroundColor: 'smokeygrey',
+        }
+        const root={
+            height: '100vh',
+            background: "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%",
+            marginBottom:'50px'
+        }
+        const Signinesee = {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItem: 'center',
+            color: 'White',
+            marginBottom: '20px',
+            backgroundColor: '#1ebea5',
+            width: '100%',
+            boxShadow: "0 5px 5px #8088888",
+            heigth: "10rem",
+            paddingTop: "48px",
+            opacity: "0.5",
+            borderBottom: "5px solid green",
+
+        }
+        const form = {
+            width: "100%",
+            marginTop: '50px'
+        }
+        const avatar = {
+            backgroundColor: 'green',
+        }
+
         return (
             <div>
                 
