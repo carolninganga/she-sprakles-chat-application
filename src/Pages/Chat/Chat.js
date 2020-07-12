0f3a2878d7984205588949dcf0ccadd12c5db15d
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-12 12:41:07 -0400

    finished login page

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import LoginString from "../Login/LoginStrings";
+import firebase from "../../Services/firebase";
+
 
 export default class Chat extends Component {
     constructor(props){
         super(props)
         this.currentUserName = localStorage.getItem(LoginString.Name)
     }
+
+    logout=()=>{
+        firebase.auth().signOut()
+        this.props.history.push('/')
+        localStorage.clear()
+        
+    }
     render() {
         return (
             <div>
                 {this.currentUserName}
+                <button onClick={this.logout}>Logout</button>
             </div>
         )
     }
