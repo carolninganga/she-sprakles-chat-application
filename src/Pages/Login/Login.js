6e3393823d6f46d1ad4d53338f0542ab197348fc
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-12 10:37:07 -0400

    imported components for login page

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import firebase from "../../Services/firebase";
+import LoginString from "../Login/LoginStrings";
+import { Card } from "react-bootstrap";
+
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLable from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import LocalOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
+import Typography from "@material-ui/core/Typography";
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
