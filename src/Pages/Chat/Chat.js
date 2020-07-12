e71a41cda13683bc621e2122d0b2e06b2e05736b
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-12 10:24:05 -0400

    added user to chat

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LoginString from "../Login/LoginStrings";
 
 export default class Chat extends Component {
     constructor(props){
         super(props)
+        this.currentUserName = localStorage.getItem(LoginString.Name)
     }
     render() {
         return (
             <div>
-                this is the chat Component
+                {this.currentUserName}
             </div>
         )
     }
 }
+
